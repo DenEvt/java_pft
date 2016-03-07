79d3352fa83bee4724815eb7f23008c0532ecea4
Денис <cadavre2@yandex.ru>
2016-03-07 17:15:45 +0300

    добавил несколько функций

--- sandbox/src/main/java/ru/train/devt/FirstProgram.java
+++ sandbox/src/main/java/ru/train/devt/FirstProgram.java
@@ -4,7 +4,27 @@ public class FirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, world!!!");
+    hi("Vasya");
+    double l = 5.2;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 5.4;
+    System.out.println("Площадь прямоугольника со сторонами " + a + b + "=" + area(a, b));
   }
+
+  public static void hi(String somebody) {
+    System.out.println("Hello, " + somebody + "!!!");
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
 
 }
 
