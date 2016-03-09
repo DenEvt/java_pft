7d063bad0016001cd455b3e45f7032980df21fc0
DenEvt <devt@corp.mango.ru>
2016-03-09 17:11:26 +0300

    коммент

--- sandbox/src/main/java/ru/train/devt/Point.java
+++ sandbox/src/main/java/ru/train/devt/Point.java
@@ -1,14 +1,12 @@
 package ru.train.devt;
 
-/**
- * Created by Kad on 08.03.2016.
- */
 public class Point {
 
-  public static void main(String[] args) {
-    Distance r = new Distance(1, 1, 3, 2);
-    System.out.println("Длинна отрезка между точкой A(" + r.ax + "," + r.ay + ")" + " и " + "точкой B(" + r.bx + "," + r.by + ")" + " = " + r.distance());
-  }
+    public static void main(String[] args) {
+//      Координаты двух точек на плоскости A(x,y) B(x,y)
+        Distance r = new Distance(1, 1, 3, 2);
+        System.out.println("Длина отрезка между точкой A(" + r.ax + "," + r.ay + ")" + " и " + "точкой B(" + r.bx + "," + r.by + ")" + " = " + r.distance());
+    }
 
 
 }
