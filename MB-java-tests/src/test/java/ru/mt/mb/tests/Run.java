73c1914500a9e7f765f503e53a2072fe16c9db3c
DenEvt <devt@corp.mango.ru>
2016-03-22 20:08:49 +0300

    эксперименты с обращением к GUI swing

--- MB-java-tests/src/test/java/ru/mt/mb/tests/Run.java
+++ MB-java-tests/src/test/java/ru/mt/mb/tests/Run.java
@@ -3,9 +3,10 @@ package ru.mt.mb.tests;
 import org.netbeans.jemmy.ClassReference;
 import org.netbeans.jemmy.QueueTool;
 import org.netbeans.jemmy.operators.*;
+import org.netbeans.jemmy.util.NameComponentChooser;
 import org.testng.annotations.Test;
 
-import javax.swing.*;
+import java.awt.*;
 
 @Test
 public class Run {
@@ -18,22 +19,28 @@ public class Run {
                 new ClassReference("mango.billing.client.Main").startApplication();
                 mainFrame = new JFrameOperator(); //"Вход в MangoBilling"
                 JDialogOperator dialog = new JDialogOperator();
-                JTextFieldOperator login = new JTextFieldOperator(dialog, 1);
+                JTextFieldOperator login = new JTextFieldOperator(dialog, new  NameComponentChooser("login"));
+//                JScrollPaneOperator pane = new JScrollPaneOperator(mainFrame);
                 login.clearText();
                 login.setText("dev");
                 JPasswordFieldOperator password = new JPasswordFieldOperator(dialog);
                 password.clearText();
                 password.setText("1");
                 new JButtonOperator(dialog, "ОК").clickMouse();
-                new JRadioButtonOperator(mainFrame, "Счет").clickMouse();
-                new JRadioButtonOperator(mainFrame, "Создания").clickMouse();
-                JTextFieldOperator find = new JTextFieldOperator(mainFrame, 2);
-                find.getAccessibleContext();
+                JComponentOperator findClient = new JComponentOperator(mainFrame);
+                new JRadioButtonOperator(findClient, "Счет").clickMouse();
+                new JRadioButtonOperator(findClient, "Создания").clickMouse();
+                new JCheckBoxOperator(findClient, "Москва").push();
+                JTextFieldOperator find = new JTextFieldOperator(findClient, new  NameComponentChooser("find"));
                 find.clearText();
-                find.setText("777");
-                new JButtonOperator(mainFrame, "Закрыть").clickMouse();
-                new JMenuBarOperator(mainFrame).pushMenuNoBlock("Файл|Найти клиента");
-                new QueueTool().waitEmpty(2000);
+                find.setText("777888");
+                JTextFieldOperator edShowCustCnt = new JTextFieldOperator(findClient, 3);
+                edShowCustCnt.clearText();
+                edShowCustCnt.setText("333");
+                Component tabClient = new JTabbedPaneOperator(findClient).selectPage("Агенты");
+//                new JButtonOperator(findClient, "Закрыть").clickMouse();
+//                new JMenuBarOperator(findClient).pushMenuNoBlock("Файл|Найти клиента");
+                new QueueTool().waitEmpty(4000);
             } catch (Exception e) {
                 e.printStackTrace();
             }
