38f1d69bb16111cb5a0eee3eec74a49b8470d66b
DenEvt <devt@corp.mango.ru>
2016-03-31 01:52:03 +0300

    1

--- MB-java-tests/src/test/java/ru/mt/mb/appmanager/ApplicationManager.java
+++ MB-java-tests/src/test/java/ru/mt/mb/appmanager/ApplicationManager.java
@@ -13,7 +13,10 @@ public class ApplicationManager {
   private MenuHelper menuHelper;
 
   public void init() throws InvocationTargetException, NoSuchMethodException, ClassNotFoundException {
-    new ClassReference("mango.billing.client.Main").startApplication();
+    new ClassReference("mango.billing.client.Main").startApplication(); //"-classpath C:\dev_dbdev_pg\mango_billing_client.jar"
+
+//    Process proc = Runtime.getRuntime().exec("C:\\dev_dbdev_pg\mango_dev_dbdev_pg_run.bat");
+
     mainFrame = new JFrameOperator();
     menuHelper = new MenuHelper(mainFrame);
     findClientHelper = new FindClientHelper(mainFrame);
